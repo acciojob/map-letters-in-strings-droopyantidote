9d863c5156fa56d8af9a1a26634102d6c181aff6
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-23 12:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const letterMap = {};
+
+  // Loop through each character in the word
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    
+    // If the letter is not already a key in the letterMap, create an array for it
+    if (!letterMap[letter]) {
+      letterMap[letter] = [];
+    }
+
+    // Push the index of the letter to its array
+    letterMap[letter].push(i);
+  }
+
+  return letterMap;
+}
+
+// Test cases
+console.log(mapLetters("dodo"));   // ➞ { d: [0, 2], o: [1, 3] }
+console.log(mapLetters("froggy")); // ➞ { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+console.log(mapLetters("grapes")); // ➞ { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
